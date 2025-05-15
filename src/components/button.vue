0c4f3312b6f39eaafe86fef7e048c0c7874f68de
itsslava <77506882+itsslava@users.noreply.github.com>
2025-05-15 17:34:15 +0300

    feat(button): add reusable Button component

---
+++ src/components/button.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+	<button class="button"><slot></slot></button>
+</template>
+
+<style scoped>
+.button {
+	border: none;
+	border-radius: 10px;
+	text-align: center;
+	padding: 14px;
+	background: var(--gradient);
+	color: var(--color-primary);
+	font-weight: 600;
+	font-size: 20px;
+	font-family: var(--font);
+	cursor: pointer;
+}
+
+.button:hover {
+	background: var(--gradient-inverted);
+	transition: all 1s ease-out;
+}
+</style>
