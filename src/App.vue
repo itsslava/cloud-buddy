d27ada56ebd53724ab98bbe04eee447df243b2f5
itsslava <77506882+itsslava@users.noreply.github.com>
2025-05-30 14:49:13 +0300

    feat(stat): add Stat component and integrate in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
 import Button from './components/button.vue';
+import Stat from './components/stat.vue';
+
+const label = 'Влажность';
+const stat = '90%	';
+
+const data = {
+	label: 'Осадки',
+	stat: '0%',
+};
 </script>
 
 <template>
-	<main class="right-panel"><Button>Сохранить</Button></main>
+	<main class="right-panel">
+		<Stat :label="label" :stat="stat" />
+		<Stat v-bind="data" />
+		<Stat label="Ветер" stat="3 м/ч" />
+		<Button>Сохранить</Button>
+	</main>
 </template>
 
 <style scoped>
---
+++ src/components/stat.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({ label: String, stat: String });
+</script>
+<template>
+	<div class="stat">
+		<div class="stat-name">{{ props.label }}</div>
+		<div class="stat-value">{{ props.stat }}</div>
+	</div>
+</template>
+<style scoped>
+.stat {
+	display: flex;
+	justify-content: space-between;
+	gap: 16px;
+
+	font-size: 28px;
+}
+.stat-name {
+	font-weight: 700;
+	text-transform: uppercase;
+}
+
+.stat-value {
+	font-weight: 500;
+}
+</style>
