ece4a569d563bab3afeb08aaafcb1566f3601785
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-13 20:24:33 +0300

    feat(ui): create CitySelect, InputView and LocationIcon components

---
+++ src/components/city-select.vue
@@ -0,0 +1,47 @@
+<script setup>
+import Button from './button.vue';
+import InputView from './input-view.vue';
+import IconLocation from '../icons/location-icon.vue';
+import { ref } from 'vue';
+import { defineEmits } from 'vue';
+
+const emit = defineEmits({
+	selectCity(payload) {
+		return payload;
+	},
+});
+
+let isEdited = ref(false);
+
+function edit() {
+	isEdited.value = true;
+}
+
+function select() {
+	isEdited.value = false;
+	emit('selectCity', 'Париж');
+}
+</script>
+<template>
+	<div class="city-select">
+		<div v-if="isEdited" class="city-input">
+			<InputView placeholder="Введите город" />
+			<Button @click="select()">Сохранить</Button>
+		</div>
+		<Button v-else @click="edit()">
+			<IconLocation />
+			Изменить город
+		</Button>
+	</div>
+</template>
+<style scoped>
+.city-input {
+	display: flex;
+	gap: 12px;
+
+	width: 420px;
+}
+.city-select {
+	width: 420px;
+}
+</style>
---
+++ src/components/input-view.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+<template><input class="input" /></template>
+<style scoped>
+.input {
+	color: var(--color-white);
+	border-radius: 10px;
+	border: none;
+	box-shadow: 1px 2px 4px 0px var(--color-bg-main);
+	background: var(--color-bg-card);
+	width: 260px;
+	height: 53px;
+	padding: 15px 20px;
+	font-size: 18px;
+}
+
+.input::placeholder {
+	color: var(--color-text-placeholder);
+}
+</style>
