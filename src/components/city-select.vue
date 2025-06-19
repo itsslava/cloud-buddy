e7e012ab6553fc3386daa250d2450735a579471b
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-19 14:35:01 +0300

    refactor(city-select): add reactive city state and model binding

--- src/components/city-select.vue
+++ src/components/city-select.vue
@@ -13,24 +13,28 @@ const emit = defineEmits({
 
 let isEdited = ref(false);
 
+let city = ref('Texas');
+
 function edit() {
 	isEdited.value = true;
 }
 
 function select() {
 	isEdited.value = false;
-	emit('selectCity', 'Париж');
+	emit('selectCity', 'Paris');
 }
 </script>
 <template>
 	<div class="city-select">
+		{{ city }}
 		<div v-if="isEdited" class="city-input">
-			<InputView placeholder="Введите город" />
-			<Button @click="select()">Сохранить</Button>
+			<!-- <InputView v-model="city" v-model:additional="city" placeholder="Enter city" /> -->
+			<InputView v-model="city" v-model:additional="city" placeholder="Enter city" />
+			<Button @click="select()">Save</Button>
 		</div>
 		<Button v-else @click="edit()">
 			<IconLocation />
-			Изменить город
+			Choose city
 		</Button>
 	</div>
 </template>
