d0eabe43017db1a70fd9768d29ace4ed48a5771d
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-22 22:59:04 +0300

    feat(app): fetch weather data from API and display forecast

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,64 @@
+<!-- eslint-disable no-undef -->
 <script setup>
 import { ref, computed } from 'vue';
+
 import Stat from './components/stat.vue';
-
+import Error from './components/error.vue';
 import CitySelect from './components/city-select.vue';
+import WeatherCard from './components/weather-card.vue';
 
-const data = ref({ humidity: 90, precipitation: 0, wind: 3 });
+const API_ENDPOINT = 'https://api.weatherapi.com/v1';
+
+let data = ref();
+let error = ref();
+let activeIndex = ref(0);
 
 const dataModified = computed(() => {
 	return [
-		{ label: 'Humidity', stat: data.value.humidity + '%' },
-		{ label: 'Precipitation', stat: data.value.precipitation + '%' },
-		{ label: 'Wind', stat: data.value.wind + ' m/h' },
+		{ label: 'Humidity', stat: data.value.current.humidity + ' %' },
+		{ label: 'Cloud', stat: data.value.current.cloud + ' %' },
+		{ label: 'Wind', stat: data.value.current.wind_mph + ' m/h' },
 	];
 });
 
-let savedCity = ref('Техас');
-
-function getCity(city) {
-	savedCity.value = city;
+async function getCity(city) {
+	const params = new URLSearchParams({
+		q: city,
+		lang: 'en',
+		key: '834aa3a2cc8a4c53a94103958230906',
+		days: 3,
+	});
+	const res = await fetch(`${API_ENDPOINT}/forecast.json?${params.toString()}`);
+	if (res.status != 200) {
+		error.value = await res.json();
+		data.value = null;
+		return;
+	}
+	error.value = null;
+	data.value = await res.json();
+	console.log(data.value);
 }
 </script>
 
 <template>
 	<main class="right-panel">
-		<Stat v-for="item in dataModified" v-bind="item" :key="item.label" />
+		<Error :error="error?.error?.message" />
+		<div v-if="data" class="weather-info">
+			<div class="weather-stat">
+				<Stat v-for="item in dataModified" v-bind="item" :key="item.label" />
+			</div>
+			<div class="weather-card-list">
+				<WeatherCard
+					v-for="(item, i) in data.forecast.forecastday"
+					:key="item.date"
+					:weather-code="item.day.condition.code"
+					:temp="item.day.avgtemp_c"
+					:date="new Date(item.date)"
+					:is-active="activeIndex == i"
+					@click="() => (activeIndex = i)"
+				/>
+			</div>
+		</div>
 		<CitySelect @select-city="getCity" />
 	</main>
 </template>
@@ -31,9 +66,22 @@ function getCity(city) {
 <style scoped>
 .right-panel {
 	background: var(--color-bg-main);
-
 	padding: 60px 50px;
-
 	border-radius: 25px;
+}
+.weather-card-list {
+	display: flex;
+	gap: 1px;
+}
+.weather-info {
+	display: flex;
+	flex-direction: column;
+	gap: 80px;
+	margin-bottom: 70px;
+}
+.weather-stat {
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
 }
 </style>
