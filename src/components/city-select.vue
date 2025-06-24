6f86954656b75b614dc87af377532b9182cf1f03
itsslava <77506882+itsslava@users.noreply.github.com>
2025-06-24 16:18:05 +0300

    refactor(city): replace event-based city passing with provide/inject

--- src/components/city-select.vue
+++ src/components/city-select.vue
@@ -2,36 +2,26 @@
 import Button from './button.vue';
 import Input from './input.vue';
 import IconLocation from '../icons/location-icon.vue';
-import { ref, onMounted } from 'vue';
-import { defineEmits } from 'vue';
+import { ref, inject } from 'vue';
+import { cityProvide } from '../constants.js';
 
 let isEdited = ref(false);
-
-let city = ref('Paris');
-
-const emit = defineEmits({
-	selectCity(payload) {
-		return payload;
-	},
-});
+const city = inject(cityProvide);
+const inputValue = ref(city.value);
 
 function edit() {
 	isEdited.value = true;
 }
 
-onMounted(() => {
-	emit('selectCity', city.value);
-});
-
 function select() {
 	isEdited.value = false;
-	emit('selectCity', city.value);
+	city.value = inputValue.value;
 }
 </script>
 <template>
 	<div class="city-select">
 		<div v-if="isEdited" class="city-input">
-			<Input v-model="city" v-focus placeholder="Enter city" @keyup.enter="select()" />
+			<Input v-model="inputValue" v-focus placeholder="Enter city" @keyup.enter="select()" />
 			<Button @click="select()">Save</Button>
 		</div>
 		<Button v-else @click="edit()">
